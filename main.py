2c7dc7b66001f8b25a6913f69e034fec1f07644e
matthewjrogers <matt.rogers@dartmouth.edu>
2023-11-17 11:51:36 -0500

    fix

--- main.py
+++ main.py
@@ -24,15 +24,7 @@ app.include_router(admin.router)
 
 # specify CORS valid origins
 origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    'https://respire-dev.dartmouth.edu/'
+# FIXME: add cors valid origins
 ]
 
 app.add_middleware(
